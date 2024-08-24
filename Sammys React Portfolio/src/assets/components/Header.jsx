71e189c7f9c88b0bf3d32e6e4d2715f27ef65b7c
sammy wlodawski <samwlodawski@gmail.com>
2024-08-23 23:37:08 -0400

    Adding component files and css files and beginning import/export process.

---
+++ Sammys React Portfolio/src/assets/components/Header.jsx
@@ -0,0 +1,16 @@
+import '../styles/Header.css';
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1></h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
---
+++ Sammys React Portfolio/src/assets/components/Project.jsx
@@ -0,0 +1,1 @@
+import '../styles/Project.css;'
