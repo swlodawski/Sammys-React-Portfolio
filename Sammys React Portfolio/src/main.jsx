e82aa8b69eea50d1e2d625c343d764f622ff0be3
sammy wlodawski <samwlodawski@gmail.com>
2024-09-06 18:30:08 -0400

    Adjusted the app and main files and deleted excess package.json file.

--- Sammys React Portfolio/src/main.jsx
+++ Sammys React Portfolio/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Portfolio from './Pages/Portfolio/Portfolio'
+import Resume from './Pages/Resume/Resume'
+import Contact from './Pages/Contact/Contact'
+import AboutMe from './Pages/AboutMe/AboutMe'
+import ReactDOM from "react-dom/client";
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const router = createBrowserRouter({
+  path: "/",
+  element: <App />,
+  children: [
+    {
+      index: true,
+      element: <AboutMe />
+    },
+    {
+      index: true,
+      element: <Resume />
+    },
+    {
+      index: true,
+      element: <Portfolio />
+    },
+    {
+      index: true,
+      element: <Contact />
+    },
+  ]
+})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
