3cfeb7a24ca76d0b3794c73988050333b7dfe8be
sammy wlodawski <samwlodawski@gmail.com>
2024-09-07 15:47:53 -0400

    Adding the navbar component. No styling yet.

---
+++ Sammys React Portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import './Navbar.css';
+import { Link, useLocation } from 'react-router-dom';
+
+function Navbar() {
+
+    const currentPage = useLocation().pathname
+
+    return(
+        <div>
+            <ul className='nav nav-tabs bg-nav'>
+                <li className='nav-item'>
+                    <Link to="/"
+                    className={currentPage === "/" ? 'nav-link-active' : 'nav-link'}>
+                        About Me
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to="/Contact"
+                    className={currentPage === "/Contact" ? 'nav-link-active' : 'nav-link'}>
+                        Contact
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to="/Portfolio"
+                    className={currentPage === "/Portfolio" ? 'nav-link-active' : 'nav-link'}>
+                        Portfolio
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to="/Resume"
+                    className={currentPage === "/Resume" ? 'nav-link-active' : 'nav-link'}>
+                        Resume
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar
