27c72d0b4b3ae58a4bade210e7e7cf6140aabe64
sammy wlodawski <samwlodawski@gmail.com>
2024-09-06 21:18:25 -0400

    Added the Footer with nav links.

---
+++ Sammys React Portfolio/src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import './Footer.css';
+
+function Footer () {
+
+    return(
+        
+        <div id='footer'>
+            <div className='container'>
+                <footer id='connect' className='py-3'>
+                    <h4>My Links</h4>
+                    <a href="www.linkedin.com/in/micah-wlodawski-730831312">LinkedIn</a>
+                    <a href="https://github.com/swlodawski?tab=repositories">GitHub</a>
+                </footer>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
